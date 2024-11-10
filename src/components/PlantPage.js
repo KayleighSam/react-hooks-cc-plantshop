1c7c0bff5f19b4843bada5718645e36c78d26c40
KayleighSam <samhanremson@gmail.com>
2024-11-10 19:51:37 +0300

    Update PlantPage.js

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -3,25 +3,28 @@ import PlantList from "./PlantList";
 import NewPlantForm from "./NewPlantForm";
 import Search from "./Search";
 
+
+
 //fetch plants from the db.json
 function PlantPage() {
   const [plants, setPlants] = useState([]);
+   //serach operations
+   const [searchPlant, setSearchPlant] = useState('');
+   const handleSearchChange = (s) => {
+     setSearchPlant(s);
+   };
+ 
+   const plantFilter= plants.filter((plant) =>
+     plant.name.toLowerCase().includes(searchPlant.toLowerCase())
+   );
+ 
   useEffect(() => {
     fetch("https://react-hooks-cc-plantshop-k7zu.onrender.com/plants")
       .then((res) => res.json())
       .then((data) => setPlants(data));
   }, []);
 
-  //serach operations
-  const [searchPlant, setSearchPlant] = useState('');
-  const handleSearchChange = (s) => {
-    setSearchPlant(s);
-  };
-
-  const plantFilter= plants.filter((plant) =>
-    plant.name.toLowerCase().includes(searchPlant.toLowerCase())
-  );
-
+ 
   return (
     <main>
       <NewPlantForm setPlants={setPlants} />
